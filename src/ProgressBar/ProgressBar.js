b9b8f4538150e3c6cc6f2e42b311a3efe8588a3a
Bárbara N. Sousa <53186630+Babi-dev@users.noreply.github.com>
2020-02-05 09:41:59 -0300

    Implementando funcionalidade

--- src/ProgressBar/ProgressBar.js
+++ src/ProgressBar/ProgressBar.js
@@ -1,32 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./style.scss";
 
-const Filler = props => {
-  return <div className="Filler" style={{ width: `${props.percentage}%` }} />;
+const Filler = ({ percentage }) => {
+  return <div className="Filler" style={{ width: `${percentage}%` }} />;
 };
 
-const ProgressBar = props => {
+const ProgressBar = ({ percentage }) => {
   return (
     <div className="ProgressBar">
-      <Filler percentage={props.percentage} />
+      <Filler percentage={percentage} />
     </div>
   );
 };
 
-const array = [
+const arrQuant = [
   {
-    percentage: 60
+    item: "Item 1",
+    qtd: 10,
+    qtd_print: 0
+  },
+  {
+    item: "Item 2",
+    qtd: 20,
+    qtd_print: 0
   }
 ];
-// const porcentage = 60;
 
 function ProgressBarExemple() {
+  const [percentage, setPercentage] = useState(0);
+  const response = arrQuant.reduce((total, item) => total + item.qtd, 0);
+  let total = response;
+
+  const qualquer = (i, item, total) =>
+    new Promise((resolve, reject) => {
+      setTimeout(() => {
+        console.log("Impresso", i, item, total);
+        const percentage = 100 / total;
+        setPercentage(percentage);
+        resolve();
+      }, 2000);
+    });
+
+  async function print() {
+    for (const item of arrQuant) {
+      for (let i = 0; i < item.qtd - item.qtd_print; i++) {
+        total = total - 1;
+        await qualquer(i, item, total);
+      }
+    }
+  }
   return (
     <div>
-      {array.map(e => (
-        <ProgressBar percentage={e.percentage} />
-      ))}
+      <ProgressBar percentage={percentage} />
+
+      <button onClick={print}>Imprimir</button>
     </div>
   );
 }
